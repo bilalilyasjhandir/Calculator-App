cbfb14928d1a5ab04ad0f3d0f122467dc230bd1b
bilalilyasjhandir <bilaljhandir007@gmail.com>
2025-03-22 16:13:42 +0500

    basic gui

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+calculation = " "
+
+def add_to_calculation(symbol):
+    pass
+
+def evaluate_calculation():
+    pass
+
+def clear_field():
+    pass
+
+root = tk.Tk()
+root.geometry("300x275")
+
+text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
+text_result.grid(columnspan=5)
+root.mainloop()
